045102734ee25c642163dc035831fab64336b72c
pounceandmiss <pounceandmiss@proton.me>
2026-05-18 03:56:50 -0700

    Bump rtcma

--- zippy.mk
+++ zippy.mk
@@ -88,7 +88,7 @@ RTC_SRC    := $(DEPSDIR)/libdatachannel-tcl
 # which is why `rtc` must also be in DEPS (enforced below).
 RTCMA_VER    := 0.1.0
 RTCMA_REPO   := https://github.com/pounceandmiss/rtc-ma.git
-RTCMA_COMMIT := 018b91682d60199b043d3f9ae3712acfdd7993b2
+RTCMA_COMMIT := 664879a8eabacefea988c8d021be0563aa4497ff
 RTCMA_SRC    := $(DEPSDIR)/rtc-ma
 
 # sqlite3 dir/lib suffix collapses the leading "3." of SQLITE3_VER into the "3"
